5a44992882ab86fcf545d760dd44164f94dbf1f1
hach <75221893+Blooddd@users.noreply.github.com>
2022-05-03 17:04:57 +0300

    Update logical.py

--- logical.py
+++ logical.py
@@ -1,7 +1,9 @@
 age = int(input('какой ваш возраст? '))
 gender = input('какой у вас пол ? ')
 
-if 18 <= age <= 28 and  gender == "man" or gender == "women":
+if 18 <= age <= 28 and  (gender == "man" or gender == "women"):
     print("Кандидат прошел первичную проверку ")
 else:
     print("Кандидат не подходит ") 
+
+    
